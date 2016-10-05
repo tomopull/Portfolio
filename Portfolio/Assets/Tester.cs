e2dc85877bb5d20b9610c1af837edc8e597acaf1
tomohiro ohsawa <toohiro.ohsawa@gmail.co>
2016-10-06 01:43:21 +0900

    動画を入れる

--- Portfolio/Assets/Tester.cs
+++ Portfolio/Assets/Tester.cs
@@ -1,16 +1,52 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 
-namespace Test
-{
-    public class Tester : MonoBehaviour
-    {
-        [SerializeField] Button button;
 
-        void Start()
-        {
-            button.onClick.AddListener(() => Debug.Log("button!"));
+public class Tester : MonoBehaviour
+{
+     public string _movieFile;
+
+        // Use this for initialization
+        void Start () {
+
+            StartCoroutine(moviePlay(_movieFile));
         }
-    }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+
+        private IEnumerator moviePlay(string movieFile)
+        {
+            string  movieTexturePath    = Application.streamingAssetsPath + "/" + movieFile;
+            string  url                 = "file://" + movieTexturePath;
+            WWW     movie               = new WWW(url);
+
+            while (!movie.isDone) {
+                yield return null;
+            }
+
+            MovieTexture movieTexture = movie.movie;
+
+            while (!movieTexture.isReadyToPlay) {
+                yield return null;
+            }
+
+            var renderer = GetComponent<MeshRenderer>();
+            renderer.material.mainTexture = movieTexture;
+
+            movieTexture.loop = true;
+            movieTexture.Play ();
+
+    #if false
+            //オーディオを使用する場合はこの部分を有効にしてください
+            var audioSource = GetComponent<AudioSource>();
+            audioSource.clip = movieTexture.audioClip;
+            audioSource.loop = true;
+            audioSource.Play ();
+    #endif
+        }
 }
